3a91dd3b7ddff903ddec6135aaf88f92b34589ad
foontype <hogetype@gmail.com>
2025-03-09 14:09:56 +0000

    update

---
+++ run/devcontainer/task.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+cd $(dirname "${0}")
+
+source ../supports/bask/src/bask.sh
+
+bask_default_task="usage"
+
+task_usage() {
+    bask_list_tasks
+}
+
+task_test() {
+    cd ../..
+    bats --print-output-on-failure --pretty --recursive ./tests
+}
+
+task_tap() {
+    cd ../..
+    bats --tap ./tests
+}
